659441fea61c104412c05e5eeffb793215ec0861
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-09-20 06:34:33 +0300

    authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,16 +6,16 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
+        """require authentication
         """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """
+        """Authorization header
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """
+        """current User
         """
         return None
