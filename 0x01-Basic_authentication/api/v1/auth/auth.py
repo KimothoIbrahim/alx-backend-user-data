8d6e305500027b53a084be928cd0f29e3bd67755
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-09-20 06:29:08 +0300

    authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+from flask import request
+
+
+class Auth:
+    """Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        """
+        return None
