9d91c05bd069d5387fd45a7c96853a979fe8962a
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-09-20 07:00:21 +0300

    Check authorization:wq

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,13 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require authentication
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        if path in excluded_paths or f'{path}/' in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Authorization header
