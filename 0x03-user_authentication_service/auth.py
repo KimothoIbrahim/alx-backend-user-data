1f8145600b58135b1ecfe8006ef2b5d20d32a505
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-09-24 16:35:15 +0300

    Hash my passwords

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Auth module
+"""
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password
+    """
+    import bcrypt
+
+    salt = bcrypt.gensalt()
+
+    if password:
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
