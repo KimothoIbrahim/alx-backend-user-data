df5f8e277899b69ea65b15ff53249c449d2eaff4
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-09-24 20:03:07 +0300

    Ready

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,7 +26,8 @@ def users():
         user = AUTH.register_user(email, password)
 
         if user:
-            return jsonify({"email": f"{user.email}", "message": "user created"})
+            return jsonify({"email": f"{user.email}",
+                           "message": "user created"})
     except ValueError as err:
         return jsonify({"message": "email already registered"}), 400
 
