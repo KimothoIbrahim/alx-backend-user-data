b197ba34a20cd2231a54f86294d56fc0ea915c90
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-11-04 18:46:40 +0300

    logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import re
+import logging
+
+
+def filter_datum(fields: list[str],
+                 redaction: str,
+                 message: str,
+                 separator: str = "; "):
+    """
+    returns the log message obfuscated
+    Args:
+        fields str():
+    Returns:
+        log message
+    """
+    for data in fields:
+        pattern = rf"{data}=[^;]*"
+        message = re.sub(pattern, f'{data}={redaction}', message)
+    return message
