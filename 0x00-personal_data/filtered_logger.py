c6c49a203ff4e4c4b9f1ae67cff148605b96a150
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-11-04 19:37:06 +0300

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 import re
 import logging
-from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str,
+def filter_datum(fields: list[str], redaction: str,
                  message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     for data in fields:
